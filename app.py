8830fb1a59993142800118675d307fd719008b65
Cameron Buford <cameron.buford@FF.LOCAL>
2020-07-14 13:19:21 -0600

    working on python

---
+++ app.py
@@ -0,0 +1,32 @@
+print('Cam')
+print('o----')
+print(' ||||')
+print('*' * 10)
+
+price = 10
+
+print(price)
+price = 20
+print(price)
+
+price = 10
+rating= 4.9
+name = 'Cam'
+is_published = False
+print(price)
+
+
+full_name = "John Smith"
+age = 20
+is_new = True
+
+name = input('What is your name? ')
+print('Hi ' + name)
+
+color = input('what is your favorite color? ')
+print('Cam likes ' + color)
+
+birth_year = input('Birth year: ')
+birth_year = 1989
+age = 2019 - birth_year
+print(age)

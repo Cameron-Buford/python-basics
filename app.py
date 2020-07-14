79a0a613c798998e7b448b58e2ccefd733bdbfb5
Cameron Buford <cameron.buford@FF.LOCAL>
2020-07-14 13:50:17 -0600

    more printing in the basics with methods

--- app.py
+++ app.py
@@ -27,6 +27,44 @@ color = input('what is your favorite color? ')
 print('Cam likes ' + color)
 
 birth_year = input('Birth year: ')
-birth_year = 1989
-age = 2019 - birth_year
+print(type(birth_year))
+age = 2019 - int(birth_year)
+print(type(age))
 print(age)
+
+weight_lbs = input('weight (lbs): ')
+weight_kg = int(weight_lbs) * 0.45
+print(weight_kg)
+
+course = "python's course for beginners"
+print(course)
+
+
+course = ''' 
+Hi Cam,
+this is the first email
+thank you
+'''
+
+print(course)
+
+print(course[3])
+print(course[0])
+
+print(course[0:4])
+
+print(course[1:5])
+print(course[:])
+
+name = 'jennifer'
+print(name[1:-1])
+
+print(len(course))
+
+print(course.upper())
+print(course.lower())
+
+print(course.find('b'))
+
+
+print(course.replace('beginners', 'noobs'))
